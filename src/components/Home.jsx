883d675073fb0927efe514c0d682e2997988be3e
unknown <TFurman47@gmail.com>
2021-08-06 19:24:38 -0500

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home(props){
     return(
         <div className="add-contact">
-            <h1>Add Contact</h1>
+            <h1>Add Contact 1234</h1>
         </div>
     )
 }
