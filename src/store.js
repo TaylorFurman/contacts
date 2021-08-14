35fbb3672b21fe9e6069a17f2941b2bb1f8d61d2
unknown <TFurman47@gmail.com>
2021-08-13 19:26:45 -0500

    update

--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import { createStore } from 'redux';
 
 import contact_reducer from './reducer.js';
 
-var store = createStore(contact_reducer +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
+//var store = createStore(contact_reducer +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+var store = createStore(contact_reducer);
 export default store;
